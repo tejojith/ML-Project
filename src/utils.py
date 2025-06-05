53828e45c4b6cd54e2b72824ef3c82288817771f
tejojith <tejojith.g@gmail.com>
2025-06-05 12:28:33 +0530

    prediction pipeline using flask web app

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,9 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+import pickle
+
 
 def save_object(file_path, obj):
 
@@ -20,13 +23,21 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(x_train, y_train, x_test, y_test, models):
+def evaluate_model(x_train, y_train, x_test, y_test, models, param):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(x_train, y_train)
+            # model.fit(x_train, y_train)
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+
 
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
@@ -37,5 +48,13 @@ def evaluate_model(x_train, y_train, x_test, y_test, models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
